072db0af5c31ef6bd8396be7d22dd2208d7a1618
Alexander Sieg <alex@xanderio.de>
2021-12-01 20:09:23 +0100

    ✨ feat: orm entity for user table

--- server/src/entity.rs
+++ server/src/entity.rs
@@ -94,3 +94,98 @@ pub mod product {
         }
     }
 }
+
+pub mod user {
+    use std::convert::TryFrom;
+
+    use common::User;
+    use sea_orm::{entity::prelude::*, ActiveValue};
+
+    #[derive(Debug, Clone, PartialEq, DeriveEntityModel)]
+    #[sea_orm(table_name = "user")]
+    pub struct Model {
+        #[sea_orm(primary_key)]
+        pub id: i32,
+        #[sea_orm(unique)]
+        pub name: String,
+        pub email: Option<String>,
+        pub created_at: DateTime,
+        pub updated_at: DateTime,
+        pub balance: i32,
+        pub active: bool,
+        pub audit: bool,
+        pub redirect: bool,
+        pub avatar: Option<i32>,
+    }
+
+    #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+    pub enum Relation {}
+
+    impl ActiveModelBehavior for ActiveModel {
+        fn before_save(self, _: bool) -> Result<Self, DbErr> {
+            Ok(Self {
+                updated_at: ActiveValue::set(chrono::Utc::now().naive_utc()),
+                ..self
+            })
+        }
+    }
+
+    impl From<Model> for User {
+        fn from(model: Model) -> Self {
+            User {
+                id: model.id,
+                name: model.name,
+                email: model.email,
+                created_at: chrono::DateTime::from_utc(model.created_at, chrono::Utc),
+                updated_at: chrono::DateTime::from_utc(model.updated_at, chrono::Utc),
+                balance: model.balance,
+                barcode: None,
+                active: model.active,
+                audit: model.audit,
+                redirect: model.redirect,
+                avatar: model.avatar,
+            }
+        }
+    }
+
+    macro_rules! unwrap_or_err {
+        ($struct:ident.$field:ident) => {
+            let $field = if !$struct.$field.is_unset() {
+                $struct.$field.unwrap()
+            } else {
+                return Err(::eyre::eyre!("field not set: {}", stringify!()));
+            };
+        };
+    }
+
+    impl TryFrom<ActiveModel> for User {
+        type Error = eyre::Error;
+
+        fn try_from(value: ActiveModel) -> Result<Self, Self::Error> {
+            unwrap_or_err!(value.id);
+            unwrap_or_err!(value.name);
+            unwrap_or_err!(value.email);
+            unwrap_or_err!(value.created_at);
+            unwrap_or_err!(value.updated_at);
+            unwrap_or_err!(value.balance);
+            unwrap_or_err!(value.active);
+            unwrap_or_err!(value.audit);
+            unwrap_or_err!(value.redirect);
+            unwrap_or_err!(value.avatar);
+
+            Ok(User {
+                id,
+                name,
+                email,
+                created_at: chrono::DateTime::from_utc(created_at, chrono::Utc),
+                updated_at: chrono::DateTime::from_utc(updated_at, chrono::Utc),
+                balance,
+                barcode: None,
+                active,
+                audit,
+                redirect,
+                avatar,
+            })
+        }
+    }
+}
